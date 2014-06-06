1bc99e8cefe262d3f2ddb47ef0a7be4f692d6c65
rtruo001 <rtruo001@ucr.edu>
2014-06-06 00:58:03 -0700

    Updated more work on the cursor portion

--- RainbowRows/RainbowRows.c
+++ RainbowRows/RainbowRows.c
@@ -137,13 +137,16 @@ int SM1_Tick(int state) {
 //This variable starts the entire process of randomizing for the LED matrix.
 //The seed would be used with the rand() function to choose Red, Green, or Blue for the color.
 signed short seed_randomize = 1;
-const unsigned char RED_LIGHT = 0;
-const unsigned char GREEN_LIGHT = 1;
-const unsigned char BLUE_LIGHT = 2;
+
+//The Red mask would be used if the red color was used in the game.
+//const unsigned char RED_LIGHT = 0;
+const unsigned char GREEN_LIGHT = 0;
+const unsigned char BLUE_LIGHT = 1;
 
 signed long col_states[8] = {0x01FFFFFF, 0x02FFFFFF, 0x04FFFFFF, 0x08FFFFFF, 0x10FFFFFF, 0x20FFFFFF, 0x40FFFFFF, 0x80FFFFFF};
 void Init_LED()
 {
+	//Restarts and initializes all the columns to their basic state.
 	col_states[0] = 0x01FFFFFF;
 	col_states[1] = 0x02FFFFFF;
 	col_states[2] = 0x04FFFFFF;
@@ -155,7 +158,7 @@ void Init_LED()
 	//This variable is initialized as an LED with all columns and rows turned off except the first column.
 	signed long var_init_LED = 0x01FFFFFF;
 	//The following variables are initialized to which bits would be Red, Green, or Blue.
-	signed long red_mask = 0x01FFFFFE;
+//	signed long red_mask = 0x01FFFFFE;
 	signed long green_mask = 0x01FEFFFF;
 	signed long blue_mask = 0x01FFFEFF;
 	signed short rand_num = 0;
@@ -165,7 +168,7 @@ void Init_LED()
 		for (unsigned char j = 0; j < 8; ++j)
 		{
 			//var_init_LED
-			if (seed_randomize >= 1000)
+			if (seed_randomize >= 10000)
 			{
 				seed_randomize = 1;
 			}
@@ -174,12 +177,21 @@ void Init_LED()
 				++seed_randomize;
 			}
 			
-			rand_num = rand() % 3;
-			if (rand_num == RED_LIGHT)
-			{
-				col_states[i] = var_init_LED & (col_states[i] & red_mask);
-			}
-			else if (rand_num == GREEN_LIGHT)
+			//The random number generated would either be 0 or 1 
+			//0 - green 
+			//1 - blue.
+			//If red was used, 0 1 and 2 would be generated. 
+			//0 - red
+			//1 - green
+			//2 - blue
+			rand_num = rand() % 2;
+		
+		//The Red mask would be used if the red color was used in the game.
+		//	if (rand_num == RED_LIGHT)
+		//	{
+		//		col_states[i] = var_init_LED & (col_states[i] & red_mask);
+		//	}
+			if (rand_num == GREEN_LIGHT)
 			{
 				col_states[i] = var_init_LED & (col_states[i] & green_mask);
 			}
@@ -187,13 +199,16 @@ void Init_LED()
 			{
 				col_states[i] = var_init_LED & (col_states[i] & blue_mask);
 			}
-
-			red_mask = (var_init_LED & 0xFF000000) | (((red_mask << 1) | 0x00000001) & 0x00FFFFFF);
+			
+		//The Red mask would be used if the red color was used in the game.
+		//	red_mask = (var_init_LED & 0xFF000000) | (((red_mask << 1) | 0x00000001) & 0x00FFFFFF);
 			green_mask = (var_init_LED & 0xFF000000) | (((green_mask << 1) | 0x00000001) & 0x00FFFFFF);
 			blue_mask = (var_init_LED & 0xFF000000) | (((blue_mask << 1) | 0x00000001) & 0x00FFFFFF);
 		}
 		var_init_LED = ((var_init_LED & 0xFF000000) << 1) | 0x00FFFFFF;
-		red_mask = (var_init_LED & 0xFF000000) | 0x00FFFFFE;
+		
+	//The Red mask would be used if the red color was used in the game.
+	//	red_mask = (var_init_LED & 0xFF000000) | 0x00FFFFFE;
 		green_mask = (var_init_LED & 0xFF000000) | 0x00FEFFFF;
 		blue_mask = (var_init_LED & 0xFF000000) | 0x00FFFEFF;
 	}
@@ -224,12 +239,13 @@ void Starting_Game()
 			}
 			else
 			{
-				seed_randomize = (seed_randomize * 2) + 1;
+				++seed_randomize;
 			}
 			if ((button & Start_Button) == 0x00)
 			{
 				//Here comes the exciting part
 				state_of_game = 1;
+				//Initializes the seed for the random.
 				srand(seed_randomize);
 				Init_LED();
 				Game_State = Game_Start;
@@ -266,11 +282,12 @@ void Starting_Game()
 
 /*************************************************************************************************
  *Function: void LED_MATRIX() 
- *Summary: Would show each column of the LED matrix, with the timer set at 2
+ *Summary: Would show each column of the LED matrix, with the timer set at 1
   the LED matrix would look like it is displaying everything at once
   but the moving rows would be quick to the invisible eye. 
- *FSM: 9 states including the init state, traversing the columns of an 8x8 RGB LED matrix.
-  Does this by setting display_lights to the row it will display.
+ *FSM: 17 states including the init state, traversing the columns of an 8x8 RGB LED matrix.
+  Does this by setting display_lights to the column it will display. Then turning off the columns
+  to prevent trailing lights. 
  *************************************************************************************************/
 //This variable would choose which column the LED Matrix would display
 signed long display_lights = 0x00000000;
@@ -289,6 +306,7 @@ enum LED_States{LED_Init,
 void LED_Matrix()
 {
 	//Transitions
+	//Goes from state column on, to column off, to the next column on. The cycle continues.
 	switch(LED_State)
 	{
 		case LED_Init:
@@ -348,6 +366,8 @@ void LED_Matrix()
 	}
 	
 	//Actions
+	//The actions sets the column state to the specific colors.
+	//After a column is displayed, the next state would turn off the LED's of the column, preventing trailing lights.
 	switch(LED_State)
 	{
 		case LED_Init:
@@ -376,6 +396,8 @@ void LED_Matrix()
 		case col7:
 			display_lights = col_states[7];
 			break;
+			
+		//Turns column of the LED's off.
 		case col0_off:
 			display_lights = 0x00FFFFFFFF;
 			break;
@@ -405,34 +427,265 @@ void LED_Matrix()
 	}
 }
 
-//counts down to when a
-unsigned short blink_count = 400;
+/****************************CURSOR FUNCTIONS**************************************
+ *We can say the column is the x axis
+ *Row can be the y axis.
+ **********************************************************************************/
+	/********************Diagram example*******************************************
+	
+	row7  B   G		...
+	row6  B   B		...
+	row5  G   G		...
+	row4  G   B		...
+y	row3  B   B		...
+	row2  B   G		...
+	row1  G   B		...
+	row0  B*  G		...
+		col0 col1 col2 col3 col4 col5 col6 col7   
+						 x
+	
+	the * symbolizes where the cursor would be.
+	so when the user pushes right, it will turn into
+	
+	
+	row7  B    G	 ...
+	row6  B    B	 ...
+	row5  G    G	 ...
+	row4  G    B	 ...
+y	row3  B    B	 ...
+	row2  B    G	 ...
+	row1  G    B     ...
+	row0  B    G*    ...
+		col0 col1 col2 col3 col4 col5 col6 col7  
+						x
+						
+	When the user pushes up or down, all the code would do is take the col_state[x]
+	and then turn on and off the colors position by manipulating the bits with y.
+	When user pushes left or right, it will just shift the col_state[x-1 or x+1].
+	**********************************************************************************/
+	
+unsigned char x = 0;
+unsigned short y = 0x01;
+
+signed long cursor_green_mask = 0x01010000;
+signed long	cursor_blue_mask = 0x01000100;
+unsigned char cursor_color = -1;
+enum Cursor_States{Cursor_Init, Wait_For_Buttons, Up_State, Down_State, Left_State, Right_State} Cursor_State;
 void Player_Cursor()
 {
-	if(1)
+	//Transitions
+	switch(Cursor_State)
 	{
-		blink_count = 400;
+		case Cursor_Init:
+			break;
+		case Wait_For_Buttons:
+			if ((col_states[x] & cursor_green_mask) == 0x01000000)
+			{
+				cursor_color = GREEN_LIGHT;
+			}
+			else if ((col_states[x] & cursor_blue_mask) == 0x01000000)
+			{
+				cursor_color = BLUE_LIGHT;
+			}
+			Cursor_State = Wait_For_Buttons;
+			if ((button & Left_Button) == 0x00)
+			{
+				if (x > 0)
+				{
+					Cursor_State = Left_State;
+					--x;
+				}
+			}
+			else if ((button & Right_Button) == 0x00)
+			{
+				if (x < 7)
+				{
+					Cursor_State = Right_State;
+					++x;
+				}
+			}
+			else if ((button & Up_Button) == 0x00)
+			{
+				if (y < 0x80)
+				{
+					y = y << 1;
+					cursor_green_mask = ((cursor_green_mask & 0xFF00FFFF) | ((cursor_green_mask << 1) | 0x00010000)) | ((cursor_green_mask << 1) & 0xFF000000); 
+					cursor_blue_mask = ((cursor_blue_mask & 0xFFFF00FF) | ((cursor_blue_mask << 1) | 0x00000100)) | ((cursor_blue_mask << 1) & 0xFF000000); ;
+					Cursor_State = Up_State;
+				}
+			}
+			else if ((button & Down_Button) == 0x00)
+			{
+				if (y > 0x01)
+				{
+					Cursor_State = Down_State;
+					y = y >> 1;
+					cursor_green_mask = ((cursor_green_mask & 0xFF00FFFF) | ((cursor_green_mask >> 1) | 0x00800000)) | ((cursor_green_mask >> 1) & 0xFF000000);
+					cursor_blue_mask = ((cursor_blue_mask & 0xFFFF00FF) | ((cursor_blue_mask >> 1) | 0x00008000)) | ((cursor_green_mask >> 1) & 0xFF000000);
+				}
+			}
+			else
+			{
+				Cursor_State = Wait_For_Buttons;
+			}
+			break;
+		case Left_State:
+			if ((button & Left_Button) != 0x00)
+			{
+				Cursor_State = Wait_For_Buttons;
+			}
+			else
+			{
+				Cursor_State = Left_State;
+			}
+			break;
+		case Right_State:
+			if ((button & Right_Button) == 0x00)
+			{
+				Cursor_State = Right_State;
+			}
+			else
+			{
+				Cursor_State = Wait_For_Buttons;
+			}
+			break;
+		case Up_State:
+			if ((button & Up_Button) != 0x00)
+			{
+				Cursor_State = Wait_For_Buttons;
+			}
+			else
+			{
+				Cursor_State = Up_State;
+			}
+			break;
+		case Down_State:
+			if ((button & Down_Button) != 0x00)
+			{
+				Cursor_State = Wait_For_Buttons;
+			}
+			else
+			{
+				Cursor_State = Down_State;
+			}
+			break;
+		default:
+			Cursor_State = Cursor_Init;
+			break;
 	}
-	else if(1)
+	
+	//Actions
+	switch(Cursor_State)
 	{
-		blink_count = 400;
+		case Cursor_Init:
+			break;
+		case Wait_For_Buttons:
+			if (seed_randomize >= 10000)
+			{
+				//Restarts the seed to 1 for randomizing
+				seed_randomize = 1;
+			}
+			else
+			{
+				++seed_randomize;
+			}
+			break;
+		case Left_State:
+			break;
+		case Right_State:
+			break;
+		case Up_State:
+			break;
+		case Down_State:
+			break;
+		default:
+			break;
 	}
-	else
+}
+
+unsigned short BLINK_TIME = 250;
+unsigned short blink_count = 250;
+enum Blink_States {Blink_Init, Light_On, Light_Off} Blink_State;
+void Cursor_blinking()
+{
+	//Transitions
+	switch(Blink_State)
 	{
-		if (seed_randomize >= 1000)
-		{
-			//Restarts the seed to 1 for randomizing
-			seed_randomize = 1;	
-		}
-		else
-		{
-			++seed_randomize;
-		}
-		blink_count--;
-		if(blink_count <= 0)
-		{
-			
-		}
+		case Blink_Init:
+			Blink_State = Light_On;
+			break;
+		case Light_On:
+			if (blink_count <= 0)
+			{
+				blink_count = BLINK_TIME;
+				Blink_State = Light_Off;
+			}
+			else
+			{
+				--blink_count;
+				Blink_State = Light_On;
+			}
+			break;
+		case Light_Off:
+			if (blink_count <= 0)
+			{
+				blink_count = BLINK_TIME;
+				Blink_State = Light_On;
+			}
+			else
+			{
+				--blink_count;
+				Blink_State = Light_Off;
+			}
+			break;
+		default:
+			Blink_State = Blink_Init;
+			break;
+	}
+	
+	//Actions
+	switch(Blink_State)
+	{
+		case Blink_Init:
+			break;
+		case Light_On:
+			if (blink_count == BLINK_TIME)
+			{
+				if (cursor_color == GREEN_LIGHT)
+				{
+					col_states[x] = (col_states[x] & 0xFF000000) | (col_states[x] & (~cursor_green_mask & 0x00FFFFFF));
+				}
+				else if (cursor_color == BLUE_LIGHT)
+				{
+					col_states[x] = (col_states[x] & 0xFF000000) | (col_states[x] & (~cursor_blue_mask & 0x00FFFFFF));
+				}
+			}
+			break;
+		case Light_Off:
+			if ((button & Left_Button) == 0x00 || (button & Right_Button) == 0x00 || (button & Up_Button) == 0x00 || (button & Down_Button) == 0x00)
+			{
+				if (cursor_color == GREEN_LIGHT)
+				{
+					col_states[x] = (col_states[x] & 0xFF000000) | (col_states[x] & (~cursor_green_mask & 0x00FFFFFF));
+				}
+				else if (cursor_color == BLUE_LIGHT)
+				{
+					col_states[x] = (col_states[x] & 0xFF000000) | (col_states[x] & (~cursor_blue_mask & 0x00FFFFFF));
+				}
+			}
+			else if (blink_count == BLINK_TIME)
+			{
+				if (cursor_color == GREEN_LIGHT)
+				{
+					col_states[x] = (col_states[x] & 0xFF000000) | (col_states[x] | (cursor_green_mask & 0x00FFFFFF));
+				}
+				else if (cursor_color == BLUE_LIGHT)
+				{
+					col_states[x] = (col_states[x] & 0xFF000000) | (col_states[x] | (cursor_blue_mask & 0x00FFFFFF));
+				}
+			}
+		default:
+			break;
 	}
 }
 
@@ -507,6 +760,8 @@ int main(void)
 		else
 		{
 			LED_Matrix();
+			Player_Cursor();
+			Cursor_blinking();
 			transmit_data(display_lights & 0xFFFFFFFF);
 		}
 		while (!TimerFlag);
